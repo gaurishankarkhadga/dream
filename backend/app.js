c4e08f1fdd6fa7eea2aa947f8c78653a7384fe42
Gaurishankar Khadga <ggs699000@gmail.com>
2026-03-15 02:37:54 +0530

    changed cors issue

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(helmet({
 // Basic middleware with proper CORS
 app.use(cors({
   // origin: ['http://localhost:5173','https://vytexsite.netlify.app'],
-  origin: 'https://vytexsite.netlify.app',
+  origin: 'https://dream-3-frontend.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
